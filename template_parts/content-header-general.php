722f3eda8e0cb1c15d7d4b3c4f5ffacd5bd16357
Mauricio Gamboa <magalocr@gmail.com>
2015-07-14 20:36:57 -0600

    Page Template

---
+++ template_parts/content-header-general.php
@@ -0,0 +1,13 @@
+<?php while ( have_posts() ) : the_post(); ?>
+<header id="header-general" class="header">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="header-general">
+          <?php the_title('<h1>', '</h1>');?> 
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<?php endwhile; ?>
