8b3dd1a01c0e77e3052bf1d8d763b1437374dc15
Mauricio Gamboa <magalocr@gmail.com>
2015-07-16 14:01:54 -0600

    Related posts

--- public/js/app.js
+++ public/js/app.js
@@ -29,6 +29,27 @@ myApp.directive('owlSingleNav', [function () {
   };
 }]);
 
+myApp.directive('owlRelated', [function () {
+  return {
+    restrict: 'C',
+
+    link: function (scope, element) {
+      element.owlCarousel({
+        items : 2,
+        itemsCustom : false,
+        itemsDesktop : [1199,2],
+        itemsDesktopSmall : [980,2],
+        itemsTablet: [768,2],
+        itemsTabletSmall: false,
+        itemsMobile : [479,1],
+        singleItem : false,
+        itemsScaleUp : false,
+        autoPlay: true
+      });
+    }
+  };
+}]);
+
 myApp.directive('changeMenu', ['deviceDetector', '$window', '$document', function (deviceDetector, $window, $document) {
   return {
     restrict: 'A',
@@ -70,6 +91,33 @@ myApp.directive('addIcon', [function () {
       var title = element.find('.widget-title');
       if (archive) title.prepend('<span><i class="fa fa-archive"></i></span>');
       if (tags) title.prepend('<span><i class="fa fa-tag"></i></span>');
+    }
+  };
+}]);
+
+myApp.directive('addLink', [function () {
+  return {
+    restrict: 'C',
+
+    link: function (scope, element) {
+      var thumbnail = element.find('.crp_thumb');
+      var link = element.find('.crp_title').attr('href');
+      var title = element.find('.crp_title').text();
+      var excerpt = element.find('.crp_excerpt').text();
+      
+      element.html('');
+
+      element.append('<h3>' + title + '</h3>')
+
+      element.append(thumbnail)
+
+      element.append('<div><p>' + excerpt + '</p><div class="clearfix"></div></div>');
+
+      var div = element.find('.clearfix');
+
+      div.append('<a class="my-link link-1" href="">read full story <i class="fa fa-chevron-right"></i></a>')
+
+      div.find('.link-1').attr('href', link);
     }
   };
 }]);
