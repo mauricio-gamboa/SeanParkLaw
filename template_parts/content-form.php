569febf066859c62fae7b572769cdf8d27560cd2
Mauricio Gamboa <magalocr@gmail.com>
2015-07-16 16:12:23 -0600

    Contact Form

--- template_parts/content-form.php
+++ template_parts/content-form.php
@@ -5,28 +5,7 @@
     year.</strong> You will hear back from one of our attorneys the same day or next day.</p>
 
   <div class="my-form clearfix">
-    <form action="">
-      <div class="form-group">
-        <label>Your Name</label>
-        <input type="text" class="form-control">
-      </div>
-      <div class="form-group">
-        <label>Your E-mail</label>
-        <input type="email" class="form-control">
-      </div>
-      <div class="form-group">
-        <label>Cell Phone</label>
-        <input type="text" class="form-control">
-      </div>
-      <div class="form-group">
-        <label>How can we help? <span>(Optional)</span></label>
-        <textarea class="form-control" rows="5"></textarea>
-      </div>
-      <button type="submit" class="my-btn btn-2 clearfix">send message <span><i
-        class="fa fa-chevron-right"></i></span>
-      </button>
-      <img src="<?php bloginfo('template_directory'); ?>/public/images/form-image.jpg" alt=""/>
-    </form>
+    <?php echo do_shortcode('[contact-form-7 id="94" title="Contact Form"]'); ?>
   </div>
 </div>
 <div class="disclosure">
